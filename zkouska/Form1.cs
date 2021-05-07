102ba9f1cc664e0fa599471070ae8b285ab9faca
hobzat.03 <hobzat.03@B121-19>
2021-05-07 10:42:34 +0200

    sad

--- zkouska/Form1.cs
+++ zkouska/Form1.cs
@@ -21,5 +21,10 @@ namespace zkouska
         {
             MessageBox.Show("cas");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("sda");
+        }
     }
 }
